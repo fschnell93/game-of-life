19236ac54f4f7b82bd57e34dbcd9c1691b811e5e
Felicia <felicia@meepo.se>
2024-03-23 13:12:19 +0100

    Add tests and implementation for finding correct neighbours

--- src/main/java/GenerationController.java
+++ src/main/java/GenerationController.java
@@ -1,15 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenerationController {
 
     public GenerationController() {
 
     }
 
+    public static List<Index> getNeighbours(int numRows, int numColumns, int x, int y) {
+        List<Index> neighbours = new ArrayList<>();
+        for (int i = x - 1; i <= x + 1; i++) {
+            for (int j = y - 1; j <= y + 1; j++) {
+                if (i >= 0 && i < numRows && j >= 0 && j < numColumns && !(i == x && j == y)) {
+                    neighbours.add(new Index(i, j));
+                }
+            }
+        }
+        return neighbours;
+    }
+
     public static boolean shouldLiveNextGeneration(int numberOfAliveNeighbours, boolean isAlive) {
         if (isAlive) {
             if (numberOfAliveNeighbours < 2) {
                 return false;
-            }
-            else {
+            } else {
                 return numberOfAliveNeighbours == 2 || numberOfAliveNeighbours == 3;
             }
         } else {
--- src/test/java/GenerationControllerTest.java
+++ src/test/java/GenerationControllerTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GenerationControllerTest {
@@ -27,5 +29,39 @@ public class GenerationControllerTest {
     @Test
     public void givenDeadCellWithThreeLiveNeighboursThenCellShouldLive() {
         assertTrue(GenerationController.shouldLiveNextGeneration(3, false));
+    }
+
+    @Test
+    public void givenCellOnPositionZeroZeroThenReturnCorrectNeighbours() {
+        List<Index> neighbours = GenerationController.getNeighbours(3, 3, 0, 0);
+        assertEquals(3, neighbours.size());
+        assertTrue(neighbours.contains(new Index(0, 1)));
+        assertTrue(neighbours.contains(new Index(1, 0)));
+        assertTrue(neighbours.contains(new Index(1, 1)));
+    }
+
+    @Test
+    public void givenCellOnPositionOneOneThenReturnCorrectNeighbours() {
+        List<Index> neighbours = GenerationController.getNeighbours(3, 3, 1, 1);
+        assertEquals(8, neighbours.size());
+        assertTrue(neighbours.contains(new Index(0, 0)));
+        assertTrue(neighbours.contains(new Index(0, 1)));
+        assertTrue(neighbours.contains(new Index(0, 2)));
+        assertTrue(neighbours.contains(new Index(1, 0)));
+        assertTrue(neighbours.contains(new Index(1, 2)));
+        assertTrue(neighbours.contains(new Index(2, 0)));
+        assertTrue(neighbours.contains(new Index(2, 1)));
+        assertTrue(neighbours.contains(new Index(2, 2)));
+    }
+
+    @Test
+    public void givenCellOnPositionZeroOneThenReturnCorrectNeighbours() {
+        List<Index> neighbours = GenerationController.getNeighbours(3, 3, 0, 1);
+        assertEquals(5, neighbours.size());
+        assertTrue(neighbours.contains(new Index(0, 0)));
+        assertTrue(neighbours.contains(new Index(0, 2)));
+        assertTrue(neighbours.contains(new Index(1, 0)));
+        assertTrue(neighbours.contains(new Index(1, 1)));
+        assertTrue(neighbours.contains(new Index(1, 2)));
     }
 }
