2935c3f80ac5abb2a98e993be476bed67056fd4f
Felicia <felicia@meepo.se>
2024-03-24 14:29:22 +0100

    Remove unused import

--- src/test/java/se/schnell/solutions/gameOfLife/IndexTest.java
+++ src/test/java/se/schnell/solutions/gameOfLife/IndexTest.java
@@ -3,8 +3,6 @@ package se.schnell.solutions.gameOfLife;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 public class IndexTest {
     @Test
     public void givenTwoEqualIndexesThenReturnTrue() {
